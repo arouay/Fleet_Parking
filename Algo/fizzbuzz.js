b4434a1746ac62735c737c60b704fa4d910b5521
arouay <arouayabdelalim@gmail.com>
2026-03-20 22:27:18 +0100

    ALGO: extract fizzbuzz logic into reusable function && add unit tests for fizzbuzz logic

--- Algo/fizzbuzz.js
+++ Algo/fizzbuzz.js
@@ -1,11 +1,26 @@
-const input = parseInt(process.argv[2], 10);
-const n = Number.isNaN(input) ? 100 : input;
-
-for (let i = 1; i <= n; i++) {
+const getFizzBuzzValue = (number) => {
   let output = '';
 
-  if (i % 3 === 0) output += 'Fizz';
-  if (i % 5 === 0) output += 'Buzz';
+  if (number % 3 === 0) output += 'Fizz';
+  if (number % 5 === 0) output += 'Buzz';
 
-  console.log(output || i);
+  return output || number;
 }
+
+// Execute only if run directly (not during tests)
+if (require.main === module) {
+  const input = parseInt(process.argv[2], 10);
+
+  if (Number.isNaN(input)) {
+    console.log('Invalid input !');
+    return;
+  }
+
+  const maxNumber = input;
+
+  for (let i = 1; i <= maxNumber; i++) {
+    console.log(getFizzBuzzValue(i));
+  }
+}
+
+module.exports = { getFizzBuzzValue };
